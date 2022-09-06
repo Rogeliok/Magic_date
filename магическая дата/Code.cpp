9c6d42c77057df6218818f265dcb18c824f7c42d
Alexander <gluhovaleksandr0129@gmail.com>
2022-09-06 11:41:00 +0500

    fix_misstakes_1

--- магическая дата/Code.cpp
+++ магическая дата/Code.cpp
@@ -7,8 +7,8 @@ int number_of_day(int month, int year);
 
 int main() {
 	for (int year = 1901; year < 2001; year++)
-		for (int month = 1; month < 12; month++)
-			for (int day = 1; day < number_of_day(month, year); day++)
+		for (int month = 1; month < 13; month++)
+			for (int day = 1; day < number_of_day(month, year) + 1; day++)
 				if (Is_Day_Magic(day, month, year))
 					output(day, month, year);
 	return 0;
@@ -33,8 +33,8 @@ void output(int day, int month, int year) {
 
 int number_of_day(int month, int year) {
 	if (month == 2 && modul(year, 4) == 0) return 29;
-	if (month == 2) return 28;
-	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) return 31;
-	if (month == 4 || month == 6 || month == 9 || month == 11) return 30;
-	return 0;
+	else if (month == 2) return 28;
+	else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) return 31;
+	else if (month == 4 || month == 6 || month == 9 || month == 11) return 30;
+	else return 0;
 }
