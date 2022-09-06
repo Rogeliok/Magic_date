4b3913507f5bd09ee0d0b92fd7466377fea8d6fc
Alexander <gluhovaleksandr0129@gmail.com>
2022-09-06 11:46:31 +0500

    first_commit

--- магическая дата/Code.cpp
+++ магическая дата/Code.cpp
@@ -1,27 +1,7 @@
 # include <iostream>
 
-int modul(int n, int d);
-bool Is_Day_Magic(int day, int month, int year);
-void output(int day, int month, int year);
-int number_of_day(int month, int year);
-
-int main() {
-	for (int year = 1901; year < 2001; year++)
-		for (int month = 1; month < 13; month++)
-			for (int day = 1; day < number_of_day(month, year) + 1; day++)
-				if (Is_Day_Magic(day, month, year))
-					output(day, month, year);
-	return 0;
-}
-
 bool Is_Day_Magic(int day, int month, int year) {
-	return day * month == modul(year, 100);
-}
-
-int modul(int n, int d) {
-	while (n >= d)
-		n -= d;
-	return n;
+	return day * month == year % 100;
 }
 
 void output(int day, int month, int year) {
@@ -32,9 +12,18 @@ void output(int day, int month, int year) {
 }
 
 int number_of_day(int month, int year) {
-	if (month == 2 && modul(year, 4) == 0) return 29;
+	if (month == 2 && year % 4 == 0) return 29;
 	else if (month == 2) return 28;
 	else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) return 31;
 	else if (month == 4 || month == 6 || month == 9 || month == 11) return 30;
 	else return 0;
+}
+
+int main() {
+	for (int year = 1901; year < 2001; year++)
+		for (int month = 1; month < 13; month++)
+			for (int day = 1; day < number_of_day(month, year) + 1; day++)
+				if (Is_Day_Magic(day, month, year))
+					output(day, month, year);
+	return 0;
 }
